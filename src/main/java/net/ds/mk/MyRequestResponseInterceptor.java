06c62a2c20326da2e0900816c0a3a9915f0277fc
dubsnmavs <mark.kelly.ie@gmail.com>
2014-04-16 17:10:37 +1000

    Commented System.out lines and refactored logging request to file into seperate method

--- src/main/java/net/ds/mk/MyRequestResponseInterceptor.java
+++ src/main/java/net/ds/mk/MyRequestResponseInterceptor.java
@@ -4,21 +4,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
 
 import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
-import org.springframework.ws.client.WebServiceClientException;
-import org.springframework.ws.client.support.interceptor.ClientInterceptor;
 import org.springframework.ws.context.MessageContext;
 import org.springframework.ws.server.EndpointInterceptor;
 import org.springframework.ws.soap.SoapMessage;
@@ -28,36 +22,11 @@ public class MyRequestResponseInterceptor implements EndpointInterceptor {
 	public boolean handleRequest(MessageContext messageContext, Object endpoint) throws Exception {
 
 		SoapMessage soapMessage = (SoapMessage) messageContext.getRequest();
-
-		System.out.println("***handleRequest start***");
-		soapMessage.writeTo(System.out);
-		System.out.println("***handleRequest finish***");
 		
-//		try {
-//
-//			soapMessage.writeTo(System.out);
-//			ByteArrayOutputStream out = new ByteArrayOutputStream();
-//			soapMessage.writeTo(out);
-//			String strMsg = new String(out.toByteArray());
-//
-//			Source xmlInput = new StreamSource(new StringReader(strMsg));
-//			Transformer transformer = null;
-//
-//			transformer = TransformerFactory.newInstance().newTransformer();
-//
-//			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-//			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-//			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-//
-//			transformer.transform(xmlInput, new StreamResult(System.out));
-//
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (TransformerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+//		logRequestToFile(soapMessage, "request");
+		
+//		Useful for logging request when deployed on Heroku
+//		soapMessage.writeTo(System.out);
 
 		return true;
 	}
@@ -66,35 +35,10 @@ public class MyRequestResponseInterceptor implements EndpointInterceptor {
 
 		SoapMessage soapMessage = (SoapMessage) messageContext.getResponse();
 		
-		System.out.println("***handleResponse start***");
-		soapMessage.writeTo(System.out);
-		System.out.println("***handleResponse finish***");
-
-//		try {
-//
-//			soapMessage.writeTo(System.out);
-//			ByteArrayOutputStream out = new ByteArrayOutputStream();
-//			soapMessage.writeTo(out);
-//			String strMsg = new String(out.toByteArray());
-//
-//			Source xmlInput = new StreamSource(new StringReader(strMsg));
-//			Transformer transformer = null;
-//
-//			transformer = TransformerFactory.newInstance().newTransformer();
-//
-//			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
-//			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-//			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-//
-//			transformer.transform(xmlInput, new StreamResult(System.out));
-//
-//		} catch (IOException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (TransformerException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+//		logRequestToFile(soapMessage, "response");
+		
+//		Useful for logging request when deployed on Heroku
+//		soapMessage.writeTo(System.out);
 		
 		return true;
 	}
@@ -107,5 +51,32 @@ public class MyRequestResponseInterceptor implements EndpointInterceptor {
 	public void afterCompletion(MessageContext messageContext, Object endpoint, Exception ex) throws Exception {
 		// TODO Auto-generated method stub
 
+	}
+	
+	private void logRequestToFile(SoapMessage soapMessage, String method) {
+		try {
+
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			soapMessage.writeTo(out);
+			String strMsg = new String(out.toByteArray());
+
+			Source xmlInput = new StreamSource(new StringReader(strMsg));
+			Transformer transformer = null;
+
+			transformer = TransformerFactory.newInstance().newTransformer();
+
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+
+			transformer.transform(xmlInput, new StreamResult(new FileOutputStream("src/test/results/" + method + ".xml")));
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
