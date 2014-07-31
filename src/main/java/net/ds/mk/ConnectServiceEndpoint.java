7b21bd92d544f0872a875d0d24f56428d5a45784
MarkJKelly <mark.kelly.ie@gmail.com>
2014-07-31 15:58:37 +1000

    Added servlet-api to pom for compliation Request HTTP headers are no being logged to System.out in Endpoint Cleaned up logging in Interceptor

--- src/main/java/net/ds/mk/ConnectServiceEndpoint.java
+++ src/main/java/net/ds/mk/ConnectServiceEndpoint.java
@@ -1,9 +1,16 @@
 package net.ds.mk;
 
+import java.util.Enumeration;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import org.springframework.ws.transport.context.TransportContext;
+import org.springframework.ws.transport.context.TransportContextHolder;
+import org.springframework.ws.transport.http.HttpServletConnection;
 
 @Endpoint
 public class ConnectServiceEndpoint {
@@ -11,13 +18,40 @@ public class ConnectServiceEndpoint {
 	private static final String CONNECT_TARGET_NAMESPACE = "http://net/ds/mk";
 
 	@PayloadRoot(localPart = "DocuSignConnectUpdate", namespace = CONNECT_TARGET_NAMESPACE)
-	public @ResponsePayload
-	DocuSignConnectUpdateResponse docusignConnectUpdate(@RequestPayload DocuSignConnectUpdate request) {
+	public @ResponsePayload DocuSignConnectUpdateResponse docusignConnectUpdate(
+			@RequestPayload DocuSignConnectUpdate request) {
+
+		HttpServletRequest httpServletRequest = getHttpServletRequest();
+
+		Enumeration enumeration = httpServletRequest.getHeaderNames();
+
+		System.out.println();
+		System.out.println("-------------- Start Request HTTP Headers --------------");
+
+		while (enumeration.hasMoreElements()) {
+			String headerName = (String) enumeration.nextElement();
+			System.out.println("Name: [" + headerName + "], value: [" + getHttpHeaderValue(headerName) + "]");
+		}
+		
+		System.out.println("-------------- End Request HTTP Headers --------------");
 
 		DocuSignConnectUpdateResponse response = new DocuSignConnectUpdateResponse();
-		
-		response.setEnvelopeID(request.getDocuSignEnvelopeInformation().getEnvelopeStatus().getEnvelopeID());
+
+		response.setEnvelopeID(request.getDocuSignEnvelopeInformation()
+				.getEnvelopeStatus().getEnvelopeID());
 
 		return response;
+	}
+
+	protected HttpServletRequest getHttpServletRequest() {
+		TransportContext ctx = TransportContextHolder.getTransportContext();
+		return (null != ctx) ? ((HttpServletConnection) ctx.getConnection())
+				.getHttpServletRequest() : null;
+	}
+
+	protected String getHttpHeaderValue(final String headerName) {
+		HttpServletRequest httpServletRequest = getHttpServletRequest();
+		return (null != httpServletRequest) ? httpServletRequest
+				.getHeader(headerName) : null;
 	}
 }
--- src/main/java/net/ds/mk/MyRequestResponseInterceptor.java
+++ src/main/java/net/ds/mk/MyRequestResponseInterceptor.java
@@ -19,40 +19,49 @@ import org.springframework.ws.soap.SoapMessage;
 
 public class MyRequestResponseInterceptor implements EndpointInterceptor {
 
-	public boolean handleRequest(MessageContext messageContext, Object endpoint) throws Exception {
+	public boolean handleRequest(MessageContext messageContext, Object endpoint)
+			throws Exception {
 
 		SoapMessage soapMessage = (SoapMessage) messageContext.getRequest();
-		
-//		logRequestToFile(soapMessage, "request");
-		
-//		Useful for logging request when deployed on Heroku
-//		soapMessage.writeTo(System.out);
 
+		// logRequestToFile(soapMessage, "request");
+
+		// Useful for logging request when deployed on Heroku
+		System.out.println();
+		System.out.println("-------------- handleRequest SOAP Start --------------");
+		soapMessage.writeTo(System.out);
+		System.out.println();
+		System.out.println("-------------- handleRequest SOAP End --------------");
+		System.out.println();
+		
 		return true;
 	}
 
-	public boolean handleResponse(MessageContext messageContext, Object endpoint) throws Exception {
+	public boolean handleResponse(MessageContext messageContext, Object endpoint)
+			throws Exception {
 
 		SoapMessage soapMessage = (SoapMessage) messageContext.getResponse();
-		
-//		logRequestToFile(soapMessage, "response");
-		
-//		Useful for logging request when deployed on Heroku
+
+		// logRequestToFile(soapMessage, "response");
+
+		// Useful for logging request when deployed on Heroku
 //		soapMessage.writeTo(System.out);
-		
+
 		return true;
 	}
 
-	public boolean handleFault(MessageContext messageContext, Object endpoint) throws Exception {
+	public boolean handleFault(MessageContext messageContext, Object endpoint)
+			throws Exception {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	public void afterCompletion(MessageContext messageContext, Object endpoint, Exception ex) throws Exception {
+	public void afterCompletion(MessageContext messageContext, Object endpoint,
+			Exception ex) throws Exception {
 		// TODO Auto-generated method stub
 
 	}
-	
+
 	private void logRequestToFile(SoapMessage soapMessage, String method) {
 		try {
 
@@ -65,11 +74,15 @@ public class MyRequestResponseInterceptor implements EndpointInterceptor {
 
 			transformer = TransformerFactory.newInstance().newTransformer();
 
-			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+			transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION,
+					"yes");
 			transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-			transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
+			transformer.setOutputProperty(
+					"{http://xml.apache.org/xslt}indent-amount", "2");
 
-			transformer.transform(xmlInput, new StreamResult(new FileOutputStream("src/test/results/" + method + ".xml")));
+			transformer.transform(xmlInput,
+					new StreamResult(new FileOutputStream("src/test/results/"
+							+ method + ".xml")));
 
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
